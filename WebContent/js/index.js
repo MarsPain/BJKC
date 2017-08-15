83690812f5f6d640498ee49d445d9cfdc0e28991
H <1434579002@qq.com>
2017-08-15 14:21:17 +0800

    修复第二个带选项卡不能切换的BUG

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -16,20 +16,20 @@ window.onload = function(){
     }
   }
 
-  var myTab = document.getElementById("tab2"); //整个div
-  var myUI = myTab.getElementsByTagName("ul")[0]; //一个节点
-  var myLi = myUI.getElementsByTagName("li"); //数组
-  var myDiv = myTab.getElementsByTagName("div");  //数组
+  var myTab2 = document.getElementById("tab2"); //整个div
+  var myUI2 = myTab2.getElementsByTagName("ul")[0]; //一个节点
+  var myLi2 = myUI2.getElementsByTagName("li"); //数组
+  var myDiv2 = myTab2.getElementsByTagName("div");  //数组
 
-  for(var i=0; i<myLi.length; i++){
-    myLi[i].index = i;
-    myLi[i].onclick = function(){
-      for(var j=0; j<myLi.length; j++){
-        myLi[j].className = "off";
-        myDiv[j].className = "hide";
+  for(var i=0; i<myLi2.length; i++){
+    myLi2[i].index = i;
+    myLi2[i].onclick = function(){
+      for(var j=0; j<myLi2.length; j++){
+        myLi2[j].className = "off";
+        myDiv2[j].className = "hide";
       }
       this.className = "on";
-      myDiv[this.index].className = "show"
+      myDiv2[this.index].className = "show"
     }
   }
 }
